f38d35aaf62db4e19457c8eaa3c30d0d9ec409b3
nameless <name1ess404@proton.me>
2026-03-04 18:17:40 +0600

    Fix port for render

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/extract", async (req, res) => {
     const classUrl = req.query.url;
