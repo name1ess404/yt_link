756754cac7c42cd3c14761b10b9841056ba9a672
nameless <name1ess404@proton.me>
2026-03-04 19:34:45 +0600

    Use full puppeteer with auto Chromium

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.get("/extract", async (req, res) => {
 
         const browser = await puppeteer.launch({
             headless: true,
-            executablePath: "/usr/bin/chromium-browser",
             args: ["--no-sandbox", "--disable-setuid-sandbox"]
         });
 
